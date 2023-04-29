f6fe1bb6a8156d44d522156f6f5773748518bd92
Pallab6Sense <pallab@6sensehq.com>
2023-04-29 21:10:09 +0600

    moved all the main logic in a controller folder

--- routes/contactRouter/contactRouter.js
+++ routes/contactRouter/contactRouter.js
@@ -8,14 +8,8 @@ const {
   deleteContact,
 } = require("../../controllers/contactContoller/contactController");
 
-router.route("/").post(createContact);
+router.route("/").post(createContact).get(getAllContacts);
 
-router.route("/").get(getAllContacts);
-
-router.route("/:id").get(getContact);
-
-router.route("/:id").put(updateContact);
-
-router.route("/:id").delete(deleteContact);
+router.route("/:id").get(getContact).put(updateContact).delete(deleteContact);
 
 module.exports = router;
