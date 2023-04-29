389dfe75c63430e13ea414ca8b3852650c7033e1
Pallab6Sense <pallab@6sensehq.com>
2023-04-29 20:50:49 +0600

    added all CRUD routes for contact

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv");
-
+const contactRouter=require('./routes/contactRouter/contactRouter')
 dotenv.config();
 const app = express();
 
 const port = process.env.PORT;
 
+
+app.use("/api/contacts",contactRouter) 
 app.listen(port, () => {
   console.log(`express server running on port ${port}`);
 });
